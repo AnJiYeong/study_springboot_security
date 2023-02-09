93fba726d1292d4f205bbd54f1cf3b2d68160e4b
AnJiYeong <memarcis2@naver.com>
2023-02-09 15:20:05 +0900

    .

--- src/main/java/com/study/study_springboot_security/controller/MemberWithAuthorityController.java
+++ src/main/java/com/study/study_springboot_security/controller/MemberWithAuthorityController.java
@@ -1,9 +1,11 @@
 package com.study.study_springboot_security.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class MemberWithAuthorityController {
     @RequestMapping(value = "/joinForm", method = RequestMethod.GET)
     public ModelAndView joinForm(ModelAndView modelAndView){
